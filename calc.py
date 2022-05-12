5164c2ab4e615db17f739b8b6e6847ba4b80f1cc
Daniel Kuzmenko <kuzmenko.dani@gmail.com>
2022-05-12 06:58:21 +0300

    Creating the window of the calculator

--- calc.py
+++ calc.py
@@ -1,1 +1,9 @@
 import tkinter as tk
+
+#Creating the class
+class Calculator:
+    def __init__(self):
+        self.window = tk.TK()
+        self.window.geometry("375x667")
+        self.window.resizeable(0,0)
+        self.window.title("Calculator")
